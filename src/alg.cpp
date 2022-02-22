4347b0df2a3eee60fa56af60657b6696ad55a8ac
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-02-22 21:24:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = 2; i <= sqrt(value); i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    for (uint64_t i = 2; ; i++) {
+        if (checkPrime(i)) {
+            count += 1;
+            if (n == count) {
+                return i;
+            }
+        }
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = value + 1; ; i++) {
+        if (checkPrime(i)) {
+            return i;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound ; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
